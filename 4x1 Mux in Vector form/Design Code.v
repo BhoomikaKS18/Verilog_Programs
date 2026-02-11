230e8d7731269e353ebf0832b4c71fed0ac2ae18
BhoomikaKS18 <bhoomikaks003@gmail.com>
2026-02-11 19:48:40 +0530

    Update Design Code.v

--- 4x1 Mux in Vector form/Design Code.v
+++ 4x1 Mux in Vector form/Design Code.v
@@ -16,7 +16,8 @@ module mux4x1_dataflow (I,S,Y);
   input  [1:0] S;
    output Y;
 assign Y = (S == 2'b00) ? I[0]:
-                  (S == 2'b01) ? I[1]:
-                  (S == 2'b10) ? I[2]:
-                                          I[3];
+           (S == 2'b01) ? I[1]:
+           (S == 2'b10) ? I[2]:
+                          I[3];
 endmodule
+
