e59df0fa4805e82babee7a3a788f15846351610a
BhoomikaKS18 <bhoomikaks003@gmail.com>
2026-02-13 12:13:43 +0530

    Update Testbench.v

--- Clock generation using Task/Testbench.v
+++ Clock generation using Task/Testbench.v
@@ -1,16 +1,10 @@
 //---------------- TESTBENCH ----------------
 `timescale 1ns/1ps
-
 module tb_clk_gen;
-
     reg rst;
     wire clk;
 
-    // Instantiate DUT
-    clk_gen dut (
-        .rst(rst),
-        .clk(clk)
-    );
+    clk_gen dut (rst,clk);
 
     // Reset sequence
     initial begin
@@ -18,16 +12,14 @@ module tb_clk_gen;
         #10 rst = 0;   // release reset after 10ns
     end
 
-    // Simulation control
-    initial begin
-        $dumpfile("clk_gen.vcd");
-        $dumpvars(0, tb_clk_gen);
-        #100 $finish;
-    end
-
-    // Monitor
     initial begin
         $monitor("Time=%0t | rst=%b | clk=%b", $time, rst, clk);
     end
-
+    
+     initial begin
+        $dumpfile("clk_gen.vcd");
+        $dumpvars(0,rst,clk);
+        #100 $finish;
+    end
 endmodule
+
