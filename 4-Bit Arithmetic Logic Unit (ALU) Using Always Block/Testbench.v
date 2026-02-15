db93ecfe9047dc3bfd084a59057a57ea234bb3ab
BhoomikaKS18 <bhoomikaks003@gmail.com>
2026-02-15 14:06:24 +0530

    Update Testbench.v

--- 4-Bit Arithmetic Logic Unit (ALU) Using Always Block/Testbench.v
+++ 4-Bit Arithmetic Logic Unit (ALU) Using Always Block/Testbench.v
@@ -4,10 +4,9 @@ module ALU_tb;
     reg [2:0] Sel;
     wire [3:0] ALU_Out;
     
-    ALU uut (.A(A),.B(B),.Sel(Sel),.ALU_Out(ALU_Out));
+    ALU dut (A,B,Sel,ALU_Out);
 
     initial begin
-
         A = 4'b0011; B = 4'b0001; Sel = 3'b000;  
    #10  A = 4'b0100; B = 4'b0010; Sel = 3'b001;  
    #10  A = 4'b1100; B = 4'b1010; Sel = 3'b010; 
@@ -19,11 +18,13 @@ module ALU_tb;
    #10  A = 4'b0101; B = 4'b0101; Sel = 3'b001; 
 $finish;
     end
+    
   initial begin 
-    $monitor("simtime=%0t,A=%b,B=%b,Sel=%b,ALU_Out=%b",$time,A,B,Sel,ALU_Out);
+      $monitor("simtime=%0t,A=%b | B=%b | Sel=%b | ALU_Out=%b",$time,A,B,Sel,ALU_Out);
   end
+    
 initial begin
   $dumpfile("dump.vcd");
   $dumpvars(0,A,B,Sel,ALU_Out);
 end
-endmodule
+    endmodule
