b36ee29eee4c13cca3472a256269c7f92823726d
BhoomikaKS18 <bhoomikaks003@gmail.com>
2026-02-15 14:20:57 +0530

    Update Testbench.v

--- 3-bit Gray to Binary converter/Testbench.v
+++ 3-bit Gray to Binary converter/Testbench.v
@@ -1,4 +1,4 @@
-TESTBENCH
+********************TESTBENCH****************************
 
 module gray_to_binary_tb;
   reg g2, g1, g0;
@@ -15,11 +15,14 @@ module gray_to_binary_tb;
   #10;  g2=1; g1=0; g0=1; 
   #10;  g2=1; g1=0; g0=0;
   end
+  
 initial begin
   $monitor("Gray = %b%b%b |  Binary = %b%b%b",g2, g1, g0,b2, b1,b0);
   end
+  
   initial begin 
      $dumpfile("dump.vcd");
      $dumpvars(0, g2, g1, g0, b2, b1, b0);
     end  
 endmodule
+
