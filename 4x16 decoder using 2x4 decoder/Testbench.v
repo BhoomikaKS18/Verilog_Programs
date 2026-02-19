c2a39d5f3cc98e8bbf43005539b2ea2f2d77d9c5
BhoomikaKS18 <bhoomikaks003@gmail.com>
2026-02-19 18:05:26 +0530

    Update Testbench.v

--- 4x16 decoder using 2x4 decoder/Testbench.v
+++ 4x16 decoder using 2x4 decoder/Testbench.v
@@ -1,28 +1,23 @@
 `timescale 1ns/1ps
 
-module tb_dec4x16;
-
+module dec4x16_tb;
   reg [3:0] i;
   wire [15:0] d;
 
-  dec4x16 dut (
-    .i(i),
-    .d(d)
-  );
-
+  dec4x16 dut (i,d);
   integer k;
 
   initial begin
     $dumpfile("dec4x16_tb.vcd");
-    $dumpvars(0, tb_dec4x16);
+    $dumpvars(0, dec4x16_tb);
 
-       for (k = 0; k < 16; k = k + 1) begin
-      i = k;
-      #10;
-      $display("%0t\t%4b\t%016b", $time, i, d);
-    end
+       for (k = 0; k < 16; k = k + 1)
+         begin
+         i = k;
+           #10;
+           $display("%0t\t%4b\t%016b", $time, i, d);
+         end
 
     $finish;
   end
-
-endmodule
+    endmodule
