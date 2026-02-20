2b26c3c7d26eb975f270b573e847d839049a73d3
BhoomikaKS18 <bhoomikaks003@gmail.com>
2026-02-20 13:30:13 +0530

    Update Design.v

--- Synchronous and Asynchronous reset/Design.v
+++ Synchronous and Asynchronous reset/Design.v
@@ -1,9 +1,6 @@
 module sync_async_reset(
-    input wire clk,
-    input wire rst_n, 
-    output reg [7:0] counter
-);
-
+    input wire clk,rst_n, 
+    output reg [7:0] counter);
     reg reset_sync;
 
    
@@ -11,14 +8,13 @@ module sync_async_reset(
         if (!rst_n)
             reset_sync <= 1'b1; 
         else
-            reset_sync <= 1'b0; 
+            reset_sync <= 1'b0;
     end
-
+        
     always @(posedge clk) begin
         if (reset_sync)
             counter <= 8'd0;
         else
-            counter <= counter + 1'b1;
+        counter <= counter + 1'b1;
     end
-
-endmodule
+        endmodule
