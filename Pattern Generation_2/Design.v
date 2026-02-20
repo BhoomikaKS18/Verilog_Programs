920c5c4ab25d335d8d5fae44fafc1b0ed79944d9
BhoomikaKS18 <bhoomikaks003@gmail.com>
2026-02-20 13:26:58 +0530

    Update Design.v

--- Pattern Generation_2/Design.v
+++ Pattern Generation_2/Design.v
@@ -4,10 +4,11 @@ module tb;
   initial begin
     for (int i = 1; i <= 4; i++) begin
       for (int j = 1; j <= i; j++) begin
-        $write("%0d", num);
+        $write("\t%0d", num);
         num++;
       end
       $write("\n");
+      end
     end
-  end
-endmodule
+    endmodule
+
