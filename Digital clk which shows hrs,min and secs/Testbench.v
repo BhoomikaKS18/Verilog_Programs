7a59d6fd9d05b7a8ee22b043d05ed927a19ff16b
BhoomikaKS18 <bhoomikaks003@gmail.com>
2026-02-22 18:42:21 +0530

    Update Testbench.v

--- Digital clk which shows hrs,min and secs/Testbench.v
+++ Digital clk which shows hrs,min and secs/Testbench.v
@@ -1,14 +1,21 @@
-module clock_test;
+module clock_digi_tb;
  reg clk,rst;
  wire [3:0] hours;
  wire [5:0] mins,secs;
- clock_digi
-dut(clk,rst,hours,mins,secs);
+ 
+ clock_digi dut(clk,rst,hours,mins,secs);
+ 
  initial begin
- clk= 0;
- rst= 1;
- #13 rst= 0;
- #100000 $finish;
+  clk= 0;
+  rst= 1;
+  #13 rst= 0;
+  #100000 $finish;
  end
- always #5 clk=~clk;
-endmodule
+  always #5 clk=~clk;
+
+  initial begin
+ $dumpfile("dump.vcd");
+ $dumpvars(1,clk,rst,hours,mins,secs);
+ end
+   endmodule
+
