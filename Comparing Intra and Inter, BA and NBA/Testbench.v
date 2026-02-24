9fa3c6d4ef0a9571cf093583f2aa83e4455c188a
BhoomikaKS18 <bhoomikaks003@gmail.com>
2026-02-24 19:22:22 +0530

    Update Design.v

--- Comparing Intra and Inter, BA and NBA/Testbench.v
+++ Comparing Intra and Inter, BA and NBA/Testbench.v
@@ -1,3 +1,5 @@
+//Design Code for Intra delay
+
 module delay_tb; //(Non- blocking)
   reg signed [31:0] a, b, c, d;
 initial begin
@@ -7,18 +9,19 @@ initial begin
      d <= #5 3;
 end
   initial begin
-    $monitor("Simtime=%0t, a=%0d, 
-b=%0d,c=%0d, d=%0d", $time, a, b,c,d);
-end endmodule
+    $monitor("Simtime=%0t | a=%0d | b=%0d | c=%0d | d=%0d", $time, a, b,c,d);
+  end
+    endmodule
+
 module delay_tb;
   reg signed [31:0] a, b, c, d;
 initial begin
-     a = #2 0;
-   b = #3 1;
-     c = #4 2;
-     d = #5 3;
+  a = #2 0;
+  b= #3 1;
+  c = #4 2;
+  d = #5 3;
 end
   initial begin
-    $monitor("Simtime=%0t, a=%0d, 
-b=%0d,c=%0d, d=%0d", $time, a, b,c,d);
-end endmodule
+    $monitor("Simtime=%0t | a=%0d | b=%0d | c=%0d | d=%0d", $time, a, b,c,d);
+  end
+    endmodule
